6cba1f64fe972d5b51308d50dceeda69d994b6e4
Angel Junior Rocha Guerrero <75550027+angelrochauc@users.noreply.github.com>
2023-02-15 20:53:37 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
-function sumar(){
+
+function sumar() {
   var valor1 = Number(document.getElementById('niños').value);
   var valor2 = Number(document.getElementById('joven').value);
   var valor3 = Number(document.getElementById('hora').value);
@@ -10,14 +11,33 @@ function sumar(){
 
 
 
+const fecha = document.getElementById("fecha"),
+  servicio = document.getElementById("servicio")
 
-var hoy = new Date()
-var fecha = hoy.getFullYear() + '-' + ('0' + (hoy.getMonth() + 1)).slice(-2) + '-' + ('0' + hoy.getDate()).slice(-2) ;
-var hora = ('0' + hoy.getHours()).slice(-2) + ':' + ('0' + hoy.getMinutes()).slice(-2);
+function getCurrentDate() {
+  const currentDate = new Date(),
+      options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+  fecha.innerHTML = currentDate.toLocaleDateString('es', options)
+}
 
-var fecha = fecha +' '+ hora;
-document.getElementById('fecha').value = fecha;
+function getCurrentTime() {
+  const currentDate = new Date(),
+      hours = currentDate.getHours(),
+      minutes = formatTime(currentDate.getMinutes()),
+      seconds = formatTime(currentDate.getSeconds()),
+      formatHours = formatTime(((hours + 11) % 12 + 1)),
+      format = (hours < 12) || (hours == 24) ? 'AM' : 'PM'
+  servicio.innerHTML = `${formatHours}:${minutes}:${seconds} <small>${format}</small>`
 
+}
+
+function formatTime(value) {
+  return value < 10 ? `0${value}` : value
+}
+
+setInterval(getCurrentTime, 1000)
+
+getCurrentDate()
 document.querySelector("#submit").addEventListener("click", e => {
   e.preventDefault();
 
@@ -27,34 +47,38 @@ document.querySelector("#submit").addEventListener("click", e => {
   const cliente = document.querySelector("#niños").value;
   const joven = document.querySelector("#joven").value;
   const total = document.querySelector("#total").value;
-  const fecha = document.querySelector("#fecha").value;
-  const hora = document.querySelector("#hora").value;
+  const fecha = document.querySelector("#fecha").innerHTML;
+  const hora = document.querySelector("#hora").innerText;
   const empleado = document.querySelector("#empleado").value;
-
+  const servicio = document.querySelector("#servicio").innerText;
   const resp = document.querySelector("#respuesta");
 
   resp.classList.remove("fail");
   resp.classList.remove("send");
 
-  const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
-		*Asistencia*%0A
-		*Niños:*  
-		${cliente}%0A
-		*Jóvenes:* 
-		${joven}%0A
-		*Adultos:* 
-		${hora}%0A
-		*Total:* 
-		${total}%0A
-		*Coodinador:* 
-		${empleado}%0A
-		*Fecha:* 
-		${fecha}%0A`;
+  console.log(hora);
 
-  if (cliente === "" || fecha === "" || hora === "" || joven === "") {
-    resp.classList.add("fail");
-    resp.innerHTML = `Faltan algunos datos`;
-    return false;
+  const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+  *Asistencia*%0A
+  *Niños:*  
+  ${cliente}%0A
+  *Jovenes:* 
+  ${joven}%0A
+  *Adultos:* 
+  ${hora}%0A
+  *Total:* 
+  ${total}%0A
+  *Coodinador:* 
+  ${empleado}%0A
+  *Fecha:* 
+  ${fecha}%0A
+  *Servicio:* 
+  ${servicio}`;
+
+  if (cliente === "" || joven === "") {
+      resp.classList.add("fail");
+      resp.innerHTML = `Faltan algunos datos`;
+      return false;
   }
   resp.classList.remove("fail");
   resp.classList.add("send");
@@ -64,4 +88,3 @@ document.querySelector("#submit").addEventListener("click", e => {
 
   window.open(url);
 });
-
